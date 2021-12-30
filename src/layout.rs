4235088fe4e99cc511185d87fee737bcad61c456
Pat Shaughnessy <pat_shaughnessy@apple.com>
2021-12-30 09:39:23 -0800

    Getting closer

--- src/layout.rs
+++ src/layout.rs
@@ -24,8 +24,8 @@ pub fn render(content: String, title: Option<&String>) -> String {
           meta name="description" content="";
           meta name="author" content="";
           meta name="viewport" content="width=device-width, initial-scale=1";
-          link rel="stylesheet" href="/assets/css/normalize.css";
-          link rel="stylesheet" href="/assets/css/skeleton.css";
+          link rel="stylesheet" href="/fake/assets/css/normalize.css";
+          link rel="stylesheet" href="/fake/assets/css/skeleton.css";
           link rel="alternate" type="application/atom+xml" title={ "Pat Shaughnessy - Feed" } href="http://feeds2.feedburner.com/patshaughnessy";
           link rel="icon" type="image/png" href="images/favicon.png";
         }
@@ -40,19 +40,8 @@ pub fn render(content: String, title: Option<&String>) -> String {
               }
             }
           }
-          div class="container" {
-            div class="row" {
-              div class="one-half column" style="margin-top: 35px" {
-                (PreEscaped(content))
-                div class="row" id="copyright" {
-                  p {
-                    "Content and UI design "
-                    (PreEscaped("&copy;"))
-                    " 2022 Pat Shaughnessy"
-                  }
-                }
-              }
-            }
+          div style="margin-top: 35px" {
+            (PreEscaped(content))
           }
         }
       }
